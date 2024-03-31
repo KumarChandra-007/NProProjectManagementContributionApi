7f70178458e6326d06ce53c7e081a34a3cc34012
Abhishek Sharma <abhiz1153@gmail.com>
2024-03-31 15:17:46 +0530

    cors added

--- ContributionsApi/Program.cs
+++ ContributionsApi/Program.cs
@@ -13,14 +13,23 @@ builder.Services.AddSingleton<DapperContext>();
 builder.Services.AddTransient<IContributionRepository, ContributionRepository>();
 builder.Services.AddSingleton<IContributionsService, ContributionsService>();
 builder.Services.AddSingleton<Contributions>();
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:4200")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
-
+app.UseCors("MyAllowSpecificOrigins");
 app.UseAuthorization();
 
 app.MapControllers();
